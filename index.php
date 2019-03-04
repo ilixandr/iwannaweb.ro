61126034a35fd6a70beaeda647e946ff96e9b58c
ilixandr <ilixandr@gmail.com>
2019-03-04 15:52:55 +0200

    bootstrap CDN instead of local

--- index.php
+++ index.php
@@ -7,34 +7,31 @@
   require_once(dirname(__FILE__) . '/assets/phpres/strings.' . $language . '.php');
   $pagename = basename(__FILE__, ".php");
   $pagetitle = $txt[$pagename . "_title"];
-  $pagekeywords = $txt[$pagename . "_keywords"];
-  $pagedescription = $txt[$pagename . "_description"];
+  $pagekeywords = "\"" . $txt[$pagename . "_keywords"] . "\"";
+  $pagedescription = "\"" . $txt[$pagename . "_description"] . "\"";
 ?>
 <!DOCTYPE html>
 <html lang=<?php echo $language?>>
   <head>
-    <title><?php echo $pagetitle?></title>
-  	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  	<meta name="viewport" content="width=device-width, initial-scale=1">
-  	<meta name="keywords" content=<?php echo $pagekeywords?> />
-  	<meta name="description" content=<?php echo $pagedescription?> />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $pagetitle ?></title>
+  	<meta name="keywords" content=<?php echo $pagekeywords ?> />
+  	<meta name="description" content=<?php echo $pagedescription ?> />
   	<meta name="alexaVerifyID" content="v29ZnO1qm4DkMR-EiIIz_aOrQ00" />
   	<link href="assets/img/favicon.png" rel="icon" type="image/png">
-  	<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
   	<link rel="stylesheet" href="assets/css/main.css" type="text/css">
-  	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-  	<script src="assets/js/jquery.min.js" type="text/javascript"></script>
-  	<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
-  	<script type="text/javascript">
-  	  (() => {
-        let po = document.createElement("script"); 
-        po.type = "text/javascript"; 
-        po.async = true;
-        po.src = "https://apis.google.com/js/plusone.js?publisherid=106919341084706153999";
-        let s = document.getElementsByTagName("script")[0]; 
-        s.parentNode.insertBefore(po, s);
-      })();
-  	</script>
+    
+    
   </head>
   <body>
     <nav class="navbar navbar-inverse">
@@ -273,6 +270,18 @@
         <p>© 2013-<?php echo date("Y");?> <a href="http://www.iwannaweb.ro">iwannaweb</a> | Experience web freedom </p>
       </div>
     </footer>
+    <script type="text/javascript">
+  	  (() => {
+        let po = document.createElement("script"); 
+        po.type = "text/javascript"; 
+        po.async = true;
+        po.src = "https://apis.google.com/js/plusone.js?publisherid=106919341084706153999";
+        let s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(po, s);
+      })();
+  	</script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   </body>
 </html>
 
