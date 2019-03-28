1eca571602d2aceb66186c3ed234806085c0285e
ilixandr <ilixandr@gmail.com>
2019-03-28 21:53:55 +0200

    2019-mar-28

--- circles/index.php
+++ circles/index.php
@@ -1,19 +1,20 @@
 <?php 
-function drawCircles($percentage, $radius, $margin, $color) {
+function drawCircles($box, $percentage, $radius, $margin, $color) {
     /* Calculate x and y */
     $x = floor(-$radius * cos(deg2rad(90 + 3.6 * $percentage)));
     $y = floor(-$radius * sin(deg2rad(90 + 3.6 * $percentage)));
-    echo '<svg width="300" height="300" viewBox="0 0 300 300">
+    echo '<svg width="' . $box . '" height="' . $box . '" viewBox="0 0 ' . $box . ' ' . $box . '">
   <g transform="translate(' . ($radius + 1) . ', ' . ($radius + 1) . ')" stroke="#fff" stroke-width="1">
-    <path class="flashy" d="M0 0 0-' . $radius . 'A' . $radius . ' ' . $radius . ' 0 1 1' . ($x < 0 ? '' : ' ') . $x . ($y < 0 ? '' : ' ') . $y . 'Z" fill="' . $color . '"/>
-    <path d="M0 0' . ($x < 0 ? '' : ' ') . $x . ($y < 0 ? '' : ' ') . $y . 'A' . $radius . ' ' . $radius . ' 0 0 1 0-' . $radius . 'Z" fill="#fff"/>
+    <path class="flashy" d="M0 0 0-' . $radius . 'A' . $radius . ' ' . $radius . ' 0 ' . ($percentage > 49 ? 1 : 0) . ' 1' . ($x < 0 ? '' : ' ') . $x . ($y < 0 ? '' : ' ') . $y . 'Z" fill="' . $color . '"/>
+    <path d="M0 0' . ($x < 0 ? '' : ' ') . $x . ($y < 0 ? '' : ' ') . $y . 'A' . $radius . ' ' . $radius . ' 0 ' . ($percentage > 49 ? 0 : 1) . ' 1 0-' . $radius . 'Z" fill="#fff"/>
   </g>
   <g transform="translate(' . ($radius + 1) . ', ' . ($radius + 1) . ')" stroke="#fff" stroke-width="1">
     <path d="M0 0 0-' . ($radius - $margin) . 'A' . ($radius - $margin) . ' ' . ($radius - $margin) . ' 0 1 1-' . ($radius - $margin) . ' 0Z" fill="#fff"/>
     <path d="M0 0-' . ($radius - $margin) . ' 0A' . ($radius - $margin) . ' ' . ($radius - $margin) . ' 0 0 1 0-' . ($radius - $margin) . 'Z" fill="#fff"/>
   </g>
-  <g>
-  <text class="big" x="65" y="175" fill="' . $color . '">' . $percentage . '%</text></g>
+  <g transform="translate(' . ($radius + 1) . ', ' . ($radius + 1) . ')">
+    <text class="big" x="' . '-90' . '" y="' . 47 . '" fill="' . $color . '">' . $percentage . '%</text>
+  </g>
 </svg>';
 }
 ?>
@@ -22,6 +23,7 @@ function drawCircles($percentage, $radius, $margin, $color) {
 <head>
   <title>Test circles</title>
   <style>
+    svg {display: flex; align-items:center;}
     .big {font: italic 80px sans-serif}
     svg:hover text {fill: #F9CF00}
     svg:hover .flashy {fill: #F9CF00}
@@ -29,8 +31,8 @@ function drawCircles($percentage, $radius, $margin, $color) {
 </head>
 <body>
   <div class="target">
-    <!-- drawCircles($percentage, $radius, $margin) -->
-    <?php drawCircles(67, 149, 20, '#4484CE')?>
+    <!-- drawCircles($box,$percentage, $radius, $margin, $color) -->
+    <?php drawCircles(300, 89, 149, 20, '#4484CE')?>
   </div>
 </body>
 
